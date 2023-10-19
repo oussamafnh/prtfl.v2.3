4f8289aace6316f34d4575000f83ece784588d93
oussama <fannahoussama4@gmail.com>
2023-10-19 20:06:04 +0100

    v2.3

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -9,11 +9,6 @@ function XNavbar() {
           <LightLogo />
         </div>
         <input class="checkbox" type="checkbox" name="" id="" />
-        {/* <div class="hamburger-lines">
-          <span class="line line1"></span>
-          <span class="line line2"></span>
-          <span class="line line3"></span>
-        </div> */}
         <div class="hamburger">
           <div></div>
         </div>
